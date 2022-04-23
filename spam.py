2db231a9563c7f536b2d74ac095045b9827b4ae1
Hbi2 <104261821+Hbi2@users.noreply.github.com>
2022-04-23 20:07:49 +0700

    new py

---
+++ spam.py
@@ -0,0 +1,15 @@
+import pyautogui
+import time
+
+message =  input("masukan pesan : ")
+n = input("nominal : ")
+print("Proses")
+
+count = 5
+while(count != 0) :
+    time.sleep(1)
+    count -= 1
+
+print("Kelar")
+for i in range (0, int(n)):
+    pyautogui.typewrite(message + '\n')
